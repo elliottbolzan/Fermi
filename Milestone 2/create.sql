a0de59349c70ae85b687e5f09d795d037341e54c
Jamie Palka <jamie.palka@duke.edu>
2018-11-03 17:31:05 -0400

    connected refs to professional

--- Milestone 2/create.sql
+++ Milestone 2/create.sql
@@ -12,19 +12,20 @@ sender INTEGER NOT NULL REFERENCES person(id));
 
 CREATE TABLE referrals
 (id INTEGER NOT NULL PRIMARY KEY,
+FOREIGN KEY (professional_id) REFERENCES (professional_id),
 timestamp TIMESTAMP NOT NULL,
 status BOOLEAN NOT NULL,
 recipient INTEGER NOT NULL REFERENCES person(id),
 sender INTEGER NOT NULL REFERENCES person(id));
 
-CREATE TABLE educational_experience
+CREATE TABLE education
 (id INTEGER NOT NULL PRIMARY KEY,
 person_id INTEGER NOT NULL,
 university_name VARCHAR(256) NOT NULL,
 degree_type VARCHAR(256) NOT NULL,
 FOREIGN KEY (person_id) REFERENCES person(id));
 
-CREATE TABLE professional_experience
+CREATE TABLE professional
 (id INTEGER NOT NULL PRIMARY KEY,
 person_id INTEGER NOT NULL,
 company VARCHAR(256) NOT NULL,
