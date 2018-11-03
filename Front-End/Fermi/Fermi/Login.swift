bdce8e4899ffbd214cd114b37e1ada05164add7e
Davis Booth <davis.booth@duke.edu>
2018-11-03 02:27:52 -0400

    Created login page

---
+++ Front-End/Fermi/Fermi/Login.swift
@@ -0,0 +1,32 @@
+//
+//  Login.swift
+//  Fermi
+//
+//  Created by Davis Booth on 11/3/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    @IBOutlet weak var tintedView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tintedView.layer.cornerRadius = 10
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Front-End/Fermi/Fermi/Messages.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Fermi
+//
+//  Created by Davis Booth on 11/2/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
