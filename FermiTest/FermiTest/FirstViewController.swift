f5dcc74f0eeb9e01343fd5a37ae073167e809c86
Davis Booth <davis.booth@duke.edu>
2018-11-02 23:03:20 -0400

    sdf

---
+++ FermiTest/FermiTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FermiTest
+//
+//  Created by Davis Booth on 11/2/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FermiTest/FermiTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FermiTest
+//
+//  Created by Davis Booth on 11/2/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
