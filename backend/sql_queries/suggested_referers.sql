82193cfa01e9b33c72440b1bf26eef222cfd707f
Emily Mi <emi0807@gmail.com>
2018-11-07 21:30:57 -0500

    suggested_referers sql

--- backend/sql_queries/suggested_referers.sql
+++ backend/sql_queries/suggested_referers.sql
@@ -1,6 +1,8 @@
-SELECT EE.user_id
-FROM educational_experience AS EE
-WHERE EE.university_name = (SELECT university_name
-						FROM educational_experience
-						WHERE user_id = '1')
+--6 people with same university as user
+with view1 AS (SELECT university,person
+				FROM education
+				WHERE person = '1')
+SELECT education.person
+FROM education,view1
+WHERE (education.university=view1.university AND education.person<>view1.person)
 LIMIT 6;
