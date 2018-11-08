43a605cef0f4904480f37539ae90222997ee09c7
Emily Mi <emi0807@gmail.com>
2018-11-08 00:33:53 -0500

    changed fake data

--- backend/sql_queries/update_user.sql
+++ backend/sql_queries/update_user.sql
@@ -2,16 +2,16 @@
 
 --add row to University table if university not already in it
 INSERT INTO University (id, name)
-SELECT '10000000', 'Duke'
-WHERE NOT EXISTS (SELECT name FROM University WHERE name='Duke');
+SELECT '20000000', 'UNC'
+WHERE NOT EXISTS (SELECT name FROM University WHERE name='UNC');
 
 --add row to Company table if company not already in it
 INSERT INTO Company (id, name)
-SELECT '10000000', 'Microsoft'
-WHERE NOT EXISTS (SELECT name FROM Company WHERE name='Microsoft');
+SELECT '20000000', 'Apple'
+WHERE NOT EXISTS (SELECT name FROM Company WHERE name='Apple');
 
 --add a new education
-INSERT INTO Education VALUES ('10000000', '10000000', '10000000', 'BA', TIMESTAMP '2011-05-16 15:36:38');
+INSERT INTO Education VALUES ('20000000', '20000000', '20000000', 'BA', TIMESTAMP '2011-05-16 15:36:38');
 
 --update an existing education
 UPDATE Education
@@ -19,7 +19,7 @@ SET university='111', degree_type='BS', startdate=TIMESTAMP '2011-05-16 15:36:38
 WHERE id = '1' and person='1';
 
 --add a new job experience
-INSERT INTO Experience VALUES ('10000000', '10000000', '10000000', 'software engineer', TIMESTAMP '2011-05-16 15:36:38');
+INSERT INTO Experience VALUES ('20000000', '20000000', '12000000', 'software engineer', TIMESTAMP '2011-05-16 15:36:38');
 
 --update an existing job experience
 UPDATE Experience
