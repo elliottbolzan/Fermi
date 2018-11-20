3212aee71802c2bb68fd513473b3103f4dff1227
Elliott Bolzan <elliottbolzan@gmail.com>
2018-11-20 00:06:34 -0500

    Changes to metrics.

--- backend/sql_queries/metrics.sql
+++ backend/sql_queries/metrics.sql
@@ -1,9 +1,9 @@
 -- Generosity: how often a user refers people.
 
-SELECT generosity.id, generosity.value, ROW_NUMBER () OVER (ORDER BY value DESC) AS rank, (1 - ROW_NUMBER () OVER (ORDER BY value DESC) / (
+SELECT generosity.id, generosity.value, ROW_NUMBER () OVER (ORDER BY value DESC) AS rank, ROUND((1 - ROW_NUMBER () OVER (ORDER BY value DESC) / (
     SELECT COUNT(*)
     FROM person
-)::decimal) * 100 AS percentile
+)::decimal) * 100) AS percentile
 FROM (
     SELECT person.id, COUNT(*) AS value
     FROM person, referrals
@@ -16,10 +16,10 @@ FROM (
 
 -- Popularity: how often a user has been referred.
 
-SELECT popularity.id, popularity.value, ROW_NUMBER () OVER (ORDER BY value DESC) AS rank, (1 - ROW_NUMBER () OVER (ORDER BY value DESC) / (
+SELECT popularity.id, popularity.value, ROW_NUMBER () OVER (ORDER BY value DESC) AS rank, ROUND((1 - ROW_NUMBER () OVER (ORDER BY value DESC) / (
     SELECT COUNT(*)
     FROM person
-)::decimal) * 100 AS percentile
+)::decimal) * 100) AS percentile
 FROM (
     SELECT person.id, COUNT(*) AS value
     FROM person, referrals
