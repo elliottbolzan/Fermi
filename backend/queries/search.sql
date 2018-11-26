e919decfedcb04a0a4870b4e6893a47db77b0f12
Emily Mi <emi0807@gmail.com>
2018-11-26 18:56:44 -0500

    filtering query under search.sql

--- backend/queries/search.sql
+++ backend/queries/search.sql
@@ -1,22 +1,24 @@
-
-
---university filter
-SELECT Person.name
-FROM Person, Education, University
-WHERE Person.id = Education.person 
-AND Education.university = University.id
-AND University.name = 'Duke';
-
---company filter
-SELECT Person.name
-FROM Person, Experience, Company
-WHERE Person.id = Experience.person
-AND Experience.company = Company.id
-AND Company.name = 'Facebook';
-
-
---name filter
-SELECT name
-FROM Person
-WHERE name = 'ELLDOG';
-
+SELECT Person.id, Person.name, Company.name, University.name, generosity.percentile, popularity.percentile, impact.percentile, success.percentile
+FROM Person, Education, University, Experience, Company, generosity, popularity, impact, success, Referrals
+WHERE Person.id = Education.person AND Education.university = University.id
+AND Person.id = Experience.person AND Experience.company = Company.id
+AND Person.id = generosity.id AND Person.id = popularity.id AND Person.id = impact.id AND Person.id = success.id
+AND (Referrals.sender = Person.id OR Referrals.recipient = Person.id)
+AND EXISTS (SELECT Company.name
+				FROM Company, Experience
+				WHERE Person.id = Experience.person
+				AND Experience.company = Company.id
+				AND Person.name = 'John Smith'
+				AND Company.name = 'Facebook')
+AND Person.name = 'John Smith'
+AND EXISTS (SELECT University.name
+				FROM University, Education
+				WHERE Person.id = Education.person
+				AND Education.university = University.id
+				AND Person.name = 'John Smith'
+				AND University.name = 'Duke University')
+AND generosity.percentile >= 80
+AND popularity.percentile <= 20
+AND impact.percentile >= 90
+AND success.percentile >= 60
+ORDER BY Referrals.timestamp DESC;
