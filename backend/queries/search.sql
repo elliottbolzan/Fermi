8c21fa57bf5924b0b633ea869e2c57d41a13bdb6
Emily Mi <emi0807@gmail.com>
2018-11-26 19:07:09 -0500

    filtering query under search.sql

--- backend/queries/search.sql
+++ backend/queries/search.sql
@@ -22,3 +22,26 @@ AND popularity.percentile <= 20
 AND impact.percentile >= 90
 AND success.percentile >= 60
 ORDER BY Referrals.timestamp DESC;
+
+-- tested with:
+
+-- SELECT Person.id, Person.name, Company.name, University.name, generosity.percentile, popularity.percentile, impact.percentile, success.percentile
+-- FROM Person, Education, University, Experience, Company, generosity, popularity, impact, success, Referrals
+-- WHERE Person.id = Education.person AND Education.university = University.id
+-- AND Person.id = Experience.person AND Experience.company = Company.id
+-- AND Person.id = generosity.id AND Person.id = popularity.id AND Person.id = impact.id AND Person.id = success.id
+-- AND (Referrals.sender = Person.id OR Referrals.recipient = Person.id)
+-- AND EXISTS (SELECT Company.name
+-- 				FROM Company, Experience
+-- 				WHERE Person.id = Experience.person
+-- 				AND Experience.company = Company.id
+-- 				AND Person.name = 'Sanjuanita Devit'
+-- 				AND Company.name = 'Airbnb')
+-- AND Person.name = 'Sanjuanita Devit'
+-- AND EXISTS (SELECT University.name
+-- 				FROM University, Education
+-- 				WHERE Person.id = Education.person
+-- 				AND Education.university = University.id
+-- 				AND Person.name = 'Sanjuanita Devit'
+-- 				AND University.name = 'Wuhan University of Technology')
+-- AND impact.percentile >= 60;
