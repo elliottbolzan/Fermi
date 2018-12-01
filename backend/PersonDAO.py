d16a46f9078f4d1099cd52a47b25e6203d14752f
Thara Veeramachaneni <41715199+TharaVeera@users.noreply.github.com>
2018-12-01 16:49:18 -0500

    hi
    hi

--- backend/PersonDAO.py
+++ backend/PersonDAO.py
@@ -123,20 +123,13 @@ class PersonDAO():
         return PersonDTO.id, PersonDTO.name, compList, eduList, metList
     
     
-<<<<<<< HEAD
-    def filter(self, params):
-=======
+
 
     def filter(self, parameters):
->>>>>>> b21b215a50b8cf3b79ac672e71cb7199d147241d
+
         fd = open('sql_queries/filter.sql', 'r')
         sql = fd.read()
         fd.close()        
-    
-<<<<<<< HEAD
-=======
-
->>>>>>> b21b215a50b8cf3b79ac672e71cb7199d147241d
         qualities = ""
         for quality in params["qualities"]:
            qualities += "AND quality." + quality["name"].lower() + " >= " + str(quality["percentile"]) + "\n"
@@ -160,14 +153,12 @@ class PersonDAO():
             if conn is not None:
                 conn.close()
 
-<<<<<<< HEAD
-=======
     def queryFromFile(self, filename):
         fd = open("queries/" + filename, "r")
         sql = fd.read()
         fd.close()  
         return sql      
->>>>>>> b21b215a50b8cf3b79ac672e71cb7199d147241d
+
 
     #tester code, works
     def selectFromPerson(self):
