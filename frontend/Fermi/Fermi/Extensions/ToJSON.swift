a79ad1b4447acda7532688f0ead4e56bdbfc559b
Elliott Bolzan <elliottbolzan@gmail.com>
2018-11-30 19:24:41 -0500

    Changes.

---
+++ frontend/Fermi/Fermi/Extensions/ToJSON.swift
@@ -0,0 +1,9 @@
+//
+//  ToJSON.swift
+//  Fermi
+//
+//  Created by Elliott Bolzan on 11/22/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import Foundation
---
+++ frontend/Fermi/Fermi/Model/Order.swift
@@ -0,0 +1,9 @@
+//
+//  Order.swift
+//  Fermi
+//
+//  Created by Elliott Bolzan on 11/21/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import Foundation
