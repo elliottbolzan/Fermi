cfacad7afcc9d1a6002375c51e6a4f51c741d354
Elliott Bolzan <elliottbolzan@gmail.com>
2018-12-07 18:01:29 -0500

    Fixed BIGINTs.

--- backend/queries/create_referral.sql
+++ backend/queries/create_referral.sql
@@ -1,1 +1,10 @@
-INSERT INTO Referrals VALUES(DEFAULT, %s, %s, %s, %s, %s) RETURNING id;
+INSERT INTO Referrals AS r VALUES(DEFAULT, %s, %s, (
+    SELECT Company.id
+    FROM Company, Experience
+    WHERE Company.id = Experience.company
+    AND Experience.person = %s
+), %s, %s) RETURNING id, (
+    SELECT name
+    FROM Company
+    WHERE id = r.company
+);
