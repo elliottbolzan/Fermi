7149795b923ad459b203bf8b1a089d196a268708
Elliott Bolzan <elliottbolzan@gmail.com>
2018-12-08 18:14:42 -0500

    Committing.

---
+++ frontend/Fermi/Fermi/Controllers/Draft/DraftEducation.swift
@@ -0,0 +1,30 @@
+//
+//  DraftEducation.swift
+//  Fermi
+//
+//  Created by Elliott Bolzan on 12/8/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import UIKit
+
+class DraftEducation: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ frontend/Fermi/Fermi/Controllers/Draft/DraftExperience.swift
@@ -0,0 +1,30 @@
+//
+//  DraftExperience.swift
+//  Fermi
+//
+//  Created by Elliott Bolzan on 12/8/18.
+//  Copyright © 2018 Davis Booth. All rights reserved.
+//
+
+import UIKit
+
+class DraftExperience: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
