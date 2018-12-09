1f8c6ef2c277182e1a19b6712b6dee6a57a8453f
Elliott Bolzan <elliottbolzan@gmail.com>
2018-12-09 02:41:33 -0500

    Added all the referral backend back.

--- frontend/Fermi/Fermi/Controllers/Profile.swift
+++ frontend/Fermi/Fermi/Controllers/Profile.swift
@@ -8,170 +8,158 @@
 
 import UIKit
 
-class Profile: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+class Profile: UIViewController {
     
-    // Outlets
-    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profilePicture: PolyImageView!
     @IBOutlet weak var profileName: UILabel!
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    // person variable
-    var person: Person?
+    var person = User.shared.person!
     
-    // Data arrays
-    var educationArray: [String] = ["someValue"] {
-        didSet {
-            tableView.reloadData()
-        }
-    }
-    var experienceArray: [String] = ["someValue"] {
-        didSet {
-            tableView.reloadData()
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupName()
+        setupProfilePicture()
+        setupQualities()
+        setupExperience()
+        setupEducation()
     }
     
+    func setupView() {
+        self.view.backgroundColor = State.shared.colorFor(id: person.id)
+    }
     
+    func setupName() {
+        self.profileName.text = person.name
+        self.profileName.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        self.profileName.textColor = .white
+    }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Set selected index to 0
-        segmentedControl.selectedSegmentIndex = 0
-        
-        // Set up all Profile image stuff.
-        setupProfileImage()
-        
-        
-        profileName.text = "Davis Booth"
-        
-        // Initialize Views for segmented control
-        initializeTableView()
-        initializeCollectionView()
-        
-        
-        // Show the view for the first segmented control index.
-        collectionView.isHidden = false
-        tableView.isHidden = true
-        
-        // In order to allow switching between tabs
-        collectionView.reloadData()
-        tableView.reloadData()
+    func setupProfilePicture() {
+        Server.profilePicture(id: person.id, completion: { image in
+            self.profilePicture.imageView.image = image
+        })
+    }
+    
+    func setupQualities() {
         
     }
     
-    func initializeTableView() {
+    func setupExperience() {
         
-        // Set background transparent
-        tableView.backgroundColor = self.view.backgroundColor
+    }
+    
+    func setupEducation() {
         
-        //Set up Table View Controller
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        //Creates the nib for the table view to reference
-        let nibName1 = UINib(nibName: "educationCell", bundle: nil)
-        let nibName2 = UINib(nibName: "experienceCell", bundle: nil)
-        
-        
-        //registers the nib for use with the table views
-        tableView.register(nibName1, forCellReuseIdentifier: "educationCell")
-        tableView.register(nibName2, forCellReuseIdentifier: "experienceCell")
     }
     
-    func initializeCollectionView() {
-        
-        // Set background transparent
-        collectionView.backgroundColor = self.view.backgroundColor
-        
-        //Set up Collection View
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-    
-    
-    func setupProfileImage() {
-        profileImage.layer.cornerRadius = profileImage.frame.size.width/1.7
-        profileImage.contentMode = .scaleAspectFill
-        profileImage.clipsToBounds = true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        //self.navigationController?.isNavigationBarHidden = false
-    }
-    
-    
-    
-    // Segmented Control configuration
-    @IBAction func indexChanged(_ sender: Any) {
-        
-        if segmentedControl.selectedSegmentIndex == 0 {
-            collectionView.isHidden = false
-            collectionView.reloadData()
-            tableView.isHidden = true
-        }
-        else {
-            collectionView.isHidden = true
-            tableView.isHidden = false
-            tableView.reloadData()
-        }
-    }
-
-}
-
-// Handles all Table View stuff.
-extension Profile {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 130
-    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if segmentedControl.selectedSegmentIndex == 0 {
-            return educationArray.count
-        }
-        else {
-            return experienceArray.count
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if segmentedControl.selectedSegmentIndex == 1 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "educationCell", for: indexPath) as! educationCell
-            cell.fullInit("Duke University", year: 2021, major: "Computer Science", majorType: "B.S.")
-            cell.backgroundColor = self.view.backgroundColor
-            return cell
-        }
-        else if segmentedControl.selectedSegmentIndex == 2 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "experienceCell", for: indexPath) as! experienceCell
-            cell.fullInit("SubPar Company", date: ["February 2018", "present"], position: "VP of Being a Douche")
-            cell.backgroundColor = self.view.backgroundColor
-            return cell
-        }
-        else { return UITableViewCell() }
-    }
-}
-
-
-
-// Handles all CollectionView stuff
-extension Profile {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "statsCell", for: indexPath) as! statsCell
-        cell.fullInit(90, description: "Generosity")
-        cell.backgroundColor = self.view.backgroundColor
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let education = UIViewController.createWith(identifier: "experience", type: UINavigationController.self)
-        self.present(education, animated: true, completion: nil)
-    }
-    
+//    func initializeTableView() {
+//
+//        // Set background transparent
+//        tableView.backgroundColor = self.view.backgroundColor
+//
+//        //Set up Table View Controller
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//
+//        //Creates the nib for the table view to reference
+//        let nibName1 = UINib(nibName: "educationCell", bundle: nil)
+//        let nibName2 = UINib(nibName: "experienceCell", bundle: nil)
+//
+//
+//        //registers the nib for use with the table views
+//        tableView.register(nibName1, forCellReuseIdentifier: "educationCell")
+//        tableView.register(nibName2, forCellReuseIdentifier: "experienceCell")
+//    }
+//
+//    func initializeCollectionView() {
+//
+//        // Set background transparent
+//        collectionView.backgroundColor = self.view.backgroundColor
+//
+//        //Set up Collection View
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+//    }
+//
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        //self.navigationController?.isNavigationBarHidden = false
+//    }
+//
+//
+//
+//    // Segmented Control configuration
+//    @IBAction func indexChanged(_ sender: Any) {
+//
+//        if segmentedControl.selectedSegmentIndex == 0 {
+//            collectionView.isHidden = false
+//            collectionView.reloadData()
+//            tableView.isHidden = true
+//        }
+//        else {
+//            collectionView.isHidden = true
+//            tableView.isHidden = false
+//            tableView.reloadData()
+//        }
+//    }
+//
+//}
+//
+//// Handles all Table View stuff.
+//extension Profile {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 130
+//    }
+//
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if segmentedControl.selectedSegmentIndex == 0 {
+//            return educationArray.count
+//        }
+//        else {
+//            return experienceArray.count
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if segmentedControl.selectedSegmentIndex == 1 {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "educationCell", for: indexPath) as! educationCell
+//            cell.fullInit("Duke University", year: 2021, major: "Computer Science", majorType: "B.S.")
+//            cell.backgroundColor = self.view.backgroundColor
+//            return cell
+//        }
+//        else if segmentedControl.selectedSegmentIndex == 2 {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "experienceCell", for: indexPath) as! experienceCell
+//            cell.fullInit("SubPar Company", date: ["February 2018", "present"], position: "VP of Being a Douche")
+//            cell.backgroundColor = self.view.backgroundColor
+//            return cell
+//        }
+//        else { return UITableViewCell() }
+//    }
+//}
+//
+//
+//
+//// Handles all CollectionView stuff
+//extension Profile {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 4
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "statsCell", for: indexPath) as! statsCell
+//        cell.fullInit(90, description: "Generosity")
+//        cell.backgroundColor = self.view.backgroundColor
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let education = UIViewController.createWith(identifier: "experience", type: UINavigationController.self)
+//        self.present(education, animated: true, completion: nil)
+//    }
+//    
 }
